31ba558a16f1e8cf572f4cf7b02aeda1d60e52fe
William Dale <williambdale@gmail.com>
2018-05-07 13:55:39 -0400

    Update lodgings and bookings controller

--- MusicFestivalManager/app/controllers/bookings_controller.rb
+++ MusicFestivalManager/app/controllers/bookings_controller.rb
@@ -1,2 +1,55 @@
 class BookingsController < ApplicationController
+  before_action :find_booking, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @bookings = Booking.all
+  end
+
+  def show
+  end
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.create(booking_params)
+
+    if @booking.valid?
+      redirect_to @booking
+    else
+      flash[:errors] = @booking.errors.full_messages
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @booking.update(booking_params)
+
+    if @booking.valid?
+      redirect_to @booking
+    else
+      flash[:errors] = @booking.errors.full_messages
+      render :edit
+    end
+  end
+
+  def destroy
+    @booking.destroy
+    redirect_to bookings_path
+  end
+
+
+  private
+
+  def find_booking
+    @booking = Booking.find(params[:id])
+  end
+
+  def method_name
+    params.require(:booking).permit(:name, :favorite_genre, :arrival_date, :departure_date, :lodging_id)
+  end
 end
--- MusicFestivalManager/app/controllers/lodgings_controller.rb
+++ MusicFestivalManager/app/controllers/lodgings_controller.rb
@@ -1,2 +1,55 @@
 class LodgingsController < ApplicationController
+  before_action :find_lodging, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @lodgings = Lodging.all
+  end
+
+  def show
+  end
+
+  def new
+    @lodging = Lodging.new
+  end
+
+  def create
+    @lodging = Lodging.create(lodging_params)
+
+    if @lodging.valid?
+      redirect_to @lodging
+    else
+      flash[:errors] = @lodging.errors.full_messages
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @lodging.update(lodging_params)
+
+    if @lodging.valid?
+      redirect_to @lodging
+    else
+      flash[:errors] = @lodging.errors.full_messages
+      render :edit
+    end
+  end
+
+  def destroy
+    @lodging.destroy
+    redirect_to lodgings_path
+  end
+
+
+  private
+
+  def find_lodging
+    @lodging = Lodging.find(params[:id])
+  end
+
+  def method_name
+    params.require(:lodging).permit(:name, :favorite_genre, :arrival_date, :departure_date, :lodging_id)
+  end
 end
