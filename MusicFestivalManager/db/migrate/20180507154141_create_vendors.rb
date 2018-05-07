915302bb283d1e33bf2fd454142b0bb7c72ed47f
github username <github email address>
2018-05-07 11:53:51 -0400

    updated 1 vendor attribute

--- MusicFestivalManager/db/migrate/20180507154141_create_vendors.rb
+++ MusicFestivalManager/db/migrate/20180507154141_create_vendors.rb
@@ -2,7 +2,7 @@ class CreateVendors < ActiveRecord::Migration[5.2]
   def change
     create_table :vendors do |t|
       t.string :name
-      t.string :description
+      t.string :category
 
       t.timestamps
     end
