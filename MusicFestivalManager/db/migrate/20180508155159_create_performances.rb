ea11158c0e76f2012a617adf86e917d0a5a30258
github username <github email address>
2018-05-08 14:02:46 -0400

    created seed data for performance, artist, stage

--- MusicFestivalManager/db/migrate/20180508155159_create_performances.rb
+++ MusicFestivalManager/db/migrate/20180508155159_create_performances.rb
@@ -3,8 +3,8 @@ class CreatePerformances < ActiveRecord::Migration[5.2]
     create_table :performances do |t|
       t.integer :artist_id
       t.integer :stage_id
-      t.time :start_time
-      t.time :end_time
+      t.integer :start_time
+      t.integer :end_time
 
       t.timestamps null: false
     end
--- MusicFestivalManager/db/schema.rb
+++ MusicFestivalManager/db/schema.rb
@@ -10,13 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_08_155159) do
+ActiveRecord::Schema.define(version: 2018_05_08_171800) do
 
   create_table "artists", force: :cascade do |t|
     t.string "name"
     t.string "description"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "bookings", force: :cascade do |t|
@@ -52,8 +50,7 @@ ActiveRecord::Schema.define(version: 2018_05_08_155159) do
 
   create_table "stages", force: :cascade do |t|
     t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string "location"
   end
 
   create_table "transactions", force: :cascade do |t|
