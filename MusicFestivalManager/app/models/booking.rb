ff71213f0d8907a38736f0e85c242b7f86c87456
William Dale <williambdale@gmail.com>
2018-05-08 16:53:13 -0400

    Add artist and stage delegation to booking

--- MusicFestivalManager/app/models/booking.rb
+++ MusicFestivalManager/app/models/booking.rb
@@ -1,4 +1,11 @@
 class Booking < ApplicationRecord
   belongs_to :performance
-  belongs_to :schedule 
+  belongs_to :schedule
+  delegate :artist, to: :performance
+  delegate :stage, to: :performance
+
+  def booking_by_time
+
+  end
+
 end
