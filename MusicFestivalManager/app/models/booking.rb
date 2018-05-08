02fa562057dbe5593ea079be0397c282d0e7fb5e
William Dale <williambdale@gmail.com>
2018-05-08 16:57:57 -0400

    Add start_time and end_time delegation to Booking

--- MusicFestivalManager/app/models/booking.rb
+++ MusicFestivalManager/app/models/booking.rb
@@ -3,6 +3,8 @@ class Booking < ApplicationRecord
   belongs_to :schedule
   delegate :artist, to: :performance
   delegate :stage, to: :performance
+  delegate :start_time, to: :performance
+  delegate :end_time, to: :performance
 
   def booking_by_time
 
