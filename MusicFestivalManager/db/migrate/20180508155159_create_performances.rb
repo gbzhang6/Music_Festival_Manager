123776ef48ca81b0fd7ed5208366b09e539e217c
github username <github email address>
2018-05-08 11:59:42 -0400

    updated performances controller

--- MusicFestivalManager/db/migrate/20180508155159_create_performances.rb
+++ MusicFestivalManager/db/migrate/20180508155159_create_performances.rb
@@ -1,9 +1,10 @@
 class CreatePerformances < ActiveRecord::Migration[5.2]
   def change
     create_table :performances do |t|
-      t.string :title
       t.integer :artist_id
       t.integer :stage_id
+      t.time :start_time
+      t.time :end_time
 
       t.timestamps null: false
     end
