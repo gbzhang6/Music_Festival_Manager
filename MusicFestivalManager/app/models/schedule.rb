74047c3bc3ca8927bfd82ee9bfaaae47b58351c3
github username <github email address>
2018-05-08 10:19:23 -0400

    removed visits controller and model and updated with transactions controller and model

--- MusicFestivalManager/app/models/schedule.rb
+++ MusicFestivalManager/app/models/schedule.rb
@@ -1,8 +1,8 @@
 class Schedule < ActiveRecord::Base
   has_many :bookings
   has_many :performances, through: :bookings
-  has_many :visits
-  has_many :vendors, through: :visits
+  has_many :transactions
+  has_many :vendors, through: :transactions
   has_one :user
 
   def all_items
