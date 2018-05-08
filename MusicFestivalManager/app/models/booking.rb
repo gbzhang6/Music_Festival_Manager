696f06e296943bc474119bbb98c359b1bac2e429
github username <github email address>
2018-05-08 17:16:49 -0400

    update to bookings

--- MusicFestivalManager/app/models/booking.rb
+++ MusicFestivalManager/app/models/booking.rb
@@ -1,4 +1,17 @@
 class Booking < ApplicationRecord
   belongs_to :performance
-  belongs_to :schedule 
+  belongs_to :schedule
+
+  validate :overlapping_time_for_performances
+
+  def overlapping_time_for_performances
+    #all bookings for specific instance of user
+    self.schedule.bookings.each do |other_booking|
+      if (start_time >= other_booking.end_time) && (end_time <= other_booking.start_time)
+        redirect_to schedule_path
+      else
+        booking.errors.full_messages
+      end
+    end
+
 end
