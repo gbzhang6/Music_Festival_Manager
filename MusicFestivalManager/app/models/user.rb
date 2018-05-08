4ef892591d65899601b2ad0b2919715d325b15fa
William Dale <williambdale@gmail.com>
2018-05-08 15:59:06 -0400

    Fixed booking create issue

--- MusicFestivalManager/app/models/user.rb
+++ MusicFestivalManager/app/models/user.rb
@@ -1,14 +1,17 @@
 class User < ApplicationRecord
   has_secure_password
-  
+
   has_one :schedule
   has_many :transactions, through: :schedule
   has_many :vendors, through: :visits
+  has_many :bookings, through: :schedule
+  has_many :performances, through: :bookings
+  has_many :artists, through: :performances
 
   def all_vendors
     self.schedule.vendors
   end
 
-  
+
 
 end
