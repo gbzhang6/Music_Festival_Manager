422b0dfd95e2e56c8e806bc9fe9f54bdf4b2059d
github username <github email address>
2018-05-09 22:23:29 -0400

    cleaned up migration files

--- MusicFestivalManager/db/migrate/20180507173215_create_bookings.rb
+++ MusicFestivalManager/db/migrate/20180507173215_create_bookings.rb
@@ -2,7 +2,7 @@ class CreateBookings < ActiveRecord::Migration[5.2]
   def change
     create_table :bookings do |t|
       t.integer :performance_id
-      t.integer :user_id
+      t.integer :schedule_id
 
       t.timestamps
     end
--- MusicFestivalManager/db/schema.rb
+++ MusicFestivalManager/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_09_230159) do
+ActiveRecord::Schema.define(version: 2018_05_08_171800) do
 
   create_table "artists", force: :cascade do |t|
     t.string "name"
@@ -19,15 +19,7 @@ ActiveRecord::Schema.define(version: 2018_05_09_230159) do
 
   create_table "bookings", force: :cascade do |t|
     t.integer "performance_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.integer "schedule_id"
-  end
-
-  create_table "items", force: :cascade do |t|
-    t.string "name"
-    t.integer "price"
-    t.integer "vendor_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -53,26 +45,12 @@ ActiveRecord::Schema.define(version: 2018_05_09_230159) do
     t.string "location"
   end
 
-  create_table "transactions", force: :cascade do |t|
-    t.integer "vendor_id"
-    t.integer "schedule_id"
-    t.decimal "transaction_total"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "password_digest"
     t.string "first_name"
     t.string "last_name"
-  end
-
-  create_table "vendors", force: :cascade do |t|
-    t.string "name"
-    t.string "category"
+    t.integer "schedule_id"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
